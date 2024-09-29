5eb3356c6cf5f12e48479f303fc05c4795530f83
JCornelison <gitter@VashonDesign.com>
2024-09-29 12:15:26 -0700

    chaos state of arctree, for the record!

--- themes/eoconline/layouts/OrgChart/arc-tree.js
+++ themes/eoconline/layouts/OrgChart/arc-tree.js
@@ -1,5 +1,6 @@
 let fontSize = document.querySelector(".tree").style.fontSize;
 setFontSize(fontSize);
+let uniqueID = 0;
 
 document.querySelector("#fontSize").addEventListener("input", (e) => { setFontSize(e.target); });
 document.querySelector("#fontSize").addEventListener('keyup', (event) => {
@@ -46,22 +47,107 @@ function collapseTree() {
 
 // {{ now.UnixNano }} to create a unique ID
 
-
-
 function makeList(jsonObject, listElement) {
+
+  traverse(jsonObject);
+
+}
+
+function traverse2(o) {
+  for (var i in o) {
+    if (!!o[i] && typeof (o[i]) == "object") {
+      console.log(i, o[i]);
+      traverse(o[i]);
+    } else {
+      console.log(i, o[i]);
+    }
+  }
+}
+
+
+function traverse(jsonObj) {
+  if (jsonObj !== null && typeof jsonObj == "object") {
+    Object.entries(jsonObj).forEach(([key, value]) => {
+      // key is either an array index or object key
+      console.log("traversing: " + key + " : " + value.toString());
+      traverse(value);
+    });
+  }
+  else {
+    // jsonObj is a number or string
+    console.log("    Got # or $: " + jsonObj.toString());
+  }
+}
+
+
+function makeList2(jsonObject, listElement) {
+
   for (var i in jsonObject) {
+
     console.log("processing: " + i.toString());
-    // iterate through the array or object
-    // insert the next child into the list as a <li>
+
+    let name = "";
+    let url = "";
+    let meta = "";
+    let children = false;
+
+    if (jsonObject[i] instanceof Array) {
+      console.log("Array: " + i.toString());
+      name = i.toString();
+      children = true;
+    }
+
+
+
+
+
+
     var newLI = document.createElement('li');
 
     if (jsonObject[i] instanceof Array) {
+
       newLI.innerHTML = i + ": ARRAY";
       newLI.className = "arrayOrObject"; //add a class tag so we can style differently
     }
     else if (jsonObject[i] instanceof Object) {
+
       newLI.innerHTML = i + ": OBJECT";
       newLI.className = "arrayOrObject"; //add a class tag so we can style differently
+    }
+    else {
+      newLI.innerHTML = i + ': ' + jsonObject[i];
+    }
+
+    listElement.appendChild(newLI);
+
+
+    //insert a <ul> for nested nodes
+    if (jsonObject[i] instanceof Array || jsonObject[i] instanceof Object) {
+      var newUL = document.createElement('ul');
+      //newUL.innerHTML=i;
+      listElement.appendChild(newUL);
+      makeList(jsonObject[i], newUL);
+    }
+  }
+}
+
+
+
+
+function makeListOrig(jsonObject, listElement) {
+  for (var i in jsonObject) {
+
+    // console.log("processing: " + i.toString());
+
+    var newLI = document.createElement('li');
+
+    if (jsonObject[i] instanceof Array) {
+      newLI.innerHTML = i + ": ARRAY";
+      newLI.className = "arrayOrObject";
+    }
+    else if (jsonObject[i] instanceof Object) {
+      newLI.innerHTML = i + ": OBJECT";
+      newLI.className = "arrayOrObject";
     }
     else
       newLI.innerHTML = i + ': ' + jsonObject[i];
@@ -90,5 +176,69 @@ async function readJSONFile(file) {
 }
 
 async function fileChange(file) {
-  readJSONFile(file).then(json => makeList(json, document.getElementById('JSONunorderedList')));
+  readJSONFile(file).then(
+    json => {
+      console.log(json);
+      makeList(json, document.getElementById('unorderedList'));
+      makeListOrig(json, document.getElementById('JSONunorderedList'));
+    }
+  );
 }
+/*
+
+function validateJson(jsonFile) {
+  try {
+    JSON.parse(jsonFile);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+function downloadJson() {
+  var json = document.getElementById('JSONunorderedList').innerHTML;
+  var blob = new Blob([json], { type: "text/plain;charset=utf-8" });
+  saveAs(blob, "orgchart.json");
+}
+
+function uploadJson() {
+  var file = document.getElementById('file').files[0];
+  if (file) {
+    fileChange(file);
+  }
+}
+
+function clearJson() {
+  document.getElementById('JSONunorderedList').innerHTML = '';
+}
+
+// https://ajv.js.org/guide/getting-started.html#basic-data-validation
+// https://www.npmjs.com/package/ajv
+
+// or ESM/TypeScript import
+import Ajv from "ajv"
+// Node.js require:
+const Ajv = require("ajv")
+
+const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+
+const schema = {
+  type: "object",
+  properties: {
+    foo: { type: "integer" },
+    bar: { type: "string" },
+  },
+  required: ["foo"],
+  additionalProperties: false,
+}
+
+const data = {
+  foo: 1,
+  bar: "abc",
+}
+
+const validate = ajv.compile(schema)
+const valid = validate(data)
+if (!valid) console.log(validate.errors)
+
+*/
